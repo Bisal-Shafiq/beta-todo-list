ab5dba9e9df3e59969cf3fb42fb261d7b9ccfeb1
Bisal-Shafiq <bisalshafiq4@gmail.com>
2024-04-15 23:16:42 +0500

    beta todo list

---
+++ index.ts
@@ -0,0 +1,110 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+import chalk from "chalk";
+
+let todos : string[] = [];
+
+async function createTodo(todos:string[]){
+
+    do{
+
+
+        let answer = await inquirer.prompt
+            ({
+            type: "list",
+            message: "select an operation",
+            name: "select",
+            choices: ["Add", "Update", "View", "Delete"],
+            })
+
+
+        // Add list
+
+
+        if (answer.select == "Add") 
+            {
+                let addTodo = await inquirer.prompt
+                    ({
+                    type: "input",
+                    message: "Add itmes in the list",
+                    name: "todo",
+        
+                    });
+        
+                    todos.push(addTodo.todo);
+                    todos.forEach(todo => console.log(chalk.blue.bold.italic(todo)));
+                    //console.log(todos);
+        
+            }
+
+        //Updatelist
+        
+        if (answer.select == "Update") 
+            {
+                let UpdateTodo = await inquirer.prompt
+                ({
+                type: "list",
+                message: "Update item in the list",
+                name: "todo",
+                choices: todos.map(item => item)
+        
+            
+                });
+        
+                let addTodo = await inquirer.prompt
+                ({
+                    type: "input",
+                    message: "Add itmes in the list",
+                    name: "todo",
+        
+                });
+        
+                let newTodo = todos.filter(val => val !== UpdateTodo.todo);
+                todos = [...newTodo, addTodo.todo];
+                todos.forEach(todo => console.log(chalk.yellow.bold.italic(todo)));
+            }
+
+        //View list
+    
+        if (answer.select == "View") 
+            {
+                console.log("***TO DO LIST ***");
+                todos.forEach(todo => console.log(chalk.gray.bold.italic(todo)));
+                //console.log(todos);
+                console.log("********************");
+            
+        
+            }
+
+        //Delete list item
+
+    
+        if (answer.select == "Delete") 
+            {
+        
+                let DeleteTodo = await inquirer.prompt
+                ({
+                    type: "list",
+                    message: "Update item in the list",
+                    name: "todo",
+                    choices: todos.map(item => item)
+            
+            
+                });
+            
+                let newTodo = todos.filter(val => val !== DeleteTodo.todo);
+                todos = [...newTodo];
+                todos.forEach(todo => console.log(chalk.green.bold.italic(todo)));
+                //console.log(todos);
+            
+            }
+    
+    } 
+    
+    while(true);
+
+ }
+
+createTodo(todos);
